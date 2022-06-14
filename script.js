3b9a69b76b9bfe7c9936985036d5a6ae80d2dd99
Maximilian Tauber <86464320+MaximilianTbR@users.noreply.github.com>
2022-06-14 19:56:19 +0200

    sixth commit

--- script.js
+++ script.js
@@ -60,6 +60,10 @@ let rightQuestions = 0;
 
 let currentQuestion = 0;
 
+let audio_success = new Audio('audio/success.wav');
+
+let audio_fail = new Audio('audio/fail.mp3');
+
 function init() {
     document.getElementById("all-questions").innerHTML = questions.length;
 
@@ -68,30 +72,43 @@ function init() {
 
 function showQuestion() {
 
-    if (currentQuestion >= questions.length) { // show endscreen
-        document.getElementById("end-screen").style = '';
-        document.getElementById("question-body").style = 'display: none;';
-        document.getElementById("amount-of-questions").innerHTML = questions.length;
-        document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
-        document.getElementById("header-img").src = 'img/endscreenwin.png';
-    } else { // show question
-
-        let percent = (currentQuestion + 1) / questions.length;
-        percent = Math.round(percent * 100);
-        document.getElementById("progress-bar").innerHTML = `${percent}%`;
-        document.getElementById("progress-bar").style = `width: ${percent}%;`;
-
-        console.log('Fortschritt:', percent)
-
-        let question = questions[currentQuestion];
-
-        document.getElementById("question-number").innerHTML = currentQuestion + 1;
-        document.getElementById("questiontext").innerHTML = question['question'];
-        document.getElementById("answer_1").innerHTML = question['answer_1'];
-        document.getElementById("answer_2").innerHTML = question['answer_2'];
-        document.getElementById("answer_3").innerHTML = question['answer_3'];
-        document.getElementById("answer_4").innerHTML = question['answer_4'];
+    if (gameIsOver()) {
+        showEndscreen();
+    } else {
+        updateProgressBar();
+        updateToNextQuestion();
     }
+}
+
+function gameIsOver() {
+    return currentQuestion >= questions.length
+}
+
+function showEndscreen() {
+    document.getElementById("end-screen").style = '';
+    document.getElementById("question-body").style = 'display: none;';
+    document.getElementById("amount-of-questions").innerHTML = questions.length;
+    document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
+    document.getElementById("header-img").src = 'img/endscreenwin.png';
+}
+
+function updateToNextQuestion() {
+
+    let question = questions[currentQuestion]; // array questions [0]
+
+    document.getElementById("question-number").innerHTML = currentQuestion + 1;
+    document.getElementById("questiontext").innerHTML = question['question'];
+    document.getElementById("answer_1").innerHTML = question['answer_1'];
+    document.getElementById("answer_2").innerHTML = question['answer_2'];
+    document.getElementById("answer_3").innerHTML = question['answer_3'];
+    document.getElementById("answer_4").innerHTML = question['answer_4'];
+}
+
+function updateProgressBar() {
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById("progress-bar").innerHTML = `${percent}%`;
+    document.getElementById("progress-bar").style = `width: ${percent}%;`;
 }
 
 function answer(selection) { // Funktion wird eingeleitet
@@ -99,14 +116,22 @@ function answer(selection) { // Funktion wird eingeleitet
     let selectedQuestionNumber = selection.slice(-1); // Die Variable selectedQuestionNumber gibt den letzten Buchstaben, bzw. die letzte Zahl von der vom Nutzer ausgewählten Frage wieder (Beispiel: wenn der Nutzer answer_1 auswählt, dann ist selection.slice(-1) die Zahl 1, weil 1 der letzte Buchstabe, bzw. die letzte Zahl von answer_1 ist)
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (selectedQuestionNumber == question['right_answer']) { // If-Abfrage eingeleitet & es wird gesagt: WENN selectedQuestionNumber (also die Variable aus Zeile 80, die den letzten Buchstaben/die letzte Zahl der ausgewählten Frage wiedergibt) dieselbe Zahl ist, wie die richtige Antwort, die im JSON hinterlegt ist, dann... (nächste Zeile) 
+    if (rightAnswerSelected(selectedQuestionNumber)) { // If-Abfrage eingeleitet & es wird gesagt: WENN selectedQuestionNumber (also die Variable aus Zeile 80, die den letzten Buchstaben/die letzte Zahl der ausgewählten Frage wiedergibt) dieselbe Zahl ist, wie die richtige Antwort, die im JSON hinterlegt ist, dann... (nächste Zeile) 
         document.getElementById(selection).parentNode.classList.add('bg-success'); // zur Div der jeweilig ausgewählten Antwort wird, insofern die korrekte Antwort vom Nutzer ausgewählt worden ist, eine grüne Farbe hinzugefügt
+        audio_success.play();
         rightQuestions++; // die Variable rightQuestions wird um eines erhöht, wenn der Nutzer die jew. Frage richtig beantwortet
     } else { // wenn letzte Zahl von der von Nutzer ausgewählten Frage aber nicht mit der im JSON hinterlegten richtigen Zahl übereinstimmt, dann...(siehe nächste Zeile)
         document.getElementById(selection).parentNode.classList.add('bg-danger'); // wenn der Nutzer die falsche Antwort ausgewählt hat, wird zur Div der falsche Antwort eine rote Farbe hinzugefügt
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success'); // wenn Nutzer die falsche Antwort ausgewählt hat, wird ihm die richtige Antwort, in diesen Fall die Antwort 3, direkt als Hilfe grün angeziegt
+        audio_fail.play();
     }
     document.getElementById('next-button').disabled = false;
+}
+
+function rightAnswerSelected(selectedQuestionNumber) {
+    let question = questions[currentQuestion]; // Variable question sagt, dass die aktuelle Frage die Werte aus der Frage 0 (also der 1. Frage in normalen Zahlen) aus dem Array questions umfasst
+
+    selectedQuestionNumber == question['right_answer']
 }
 
 function nextQuestion() {
